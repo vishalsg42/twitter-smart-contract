216cd38f6b7002377ab8e63fb57f99b755966890
Vishal Gupta <vishalsg42@gmail.com>
2023-11-25 17:41:50 +0530

    Implemented all the functionality

--- contracts/TwitterContract.sol
+++ contracts/TwitterContract.sol
@@ -22,7 +22,6 @@ contract TwitterContract {
         string name; // Name of the user
         string email; // Email of the user (consider privacy implications)
     }
-    uint private tweetCount;
 
     Message[] private messages;
     User[] private users;
@@ -33,19 +32,32 @@ contract TwitterContract {
     mapping(address => address[]) private followers; // An array that stores the addresses of all followers for a specific user.
     mapping(address => address[]) private followings; // An array that stores the addresses of all users a specific user is following.
 
-    mapping(address => Tweet[]) private tweets; // An array that stores the Tweet of all users a specific.
+    // This is inefficient method but just considering it for both scenario.
+    // mapping(address => Tweet[]) private tweetsByUser; // An array that stores the Tweet of all users a specific.
+    Tweet[] private tweets; // An array that stores the Tweet of all users a specific.
+    mapping(address => uint) tweetCountByUser;
 
     mapping(address => mapping(address => bool)) private accessControl;
 
     constructor() {}
 
     modifier checkIfUserExist(address _userAddress) {
-        require(!existingUsers[msg.sender], "User already exist");
+        require(!existingUsers[_userAddress], "User already exist");
+        _;
+    }
+
+    modifier checkIfUserDoesntExist(address _userAddress) {
+        require(existingUsers[_userAddress], "User doesn't exist");
+        _;
+    }
+
+    modifier checkIfUserIsAuthorize(address _userAddress) {
+        require(accessControl[msg.sender][_userAddress], "Un-Authorize Access");
         _;
     }
 
     modifier checkIfTweetCountExceed(address _userAddress, uint _count) {
-        uint totalCount = tweets[_userAddress].length;
+        uint totalCount = tweets.length;
         require(_count <= totalCount, "Tweet Count exceeds");
         _;
     }
@@ -56,31 +68,30 @@ contract TwitterContract {
     ) public checkIfUserExist(msg.sender) {
         users.push(User(msg.sender, _name, _email));
         existingUsers[msg.sender] = true;
+        accessControl[msg.sender][msg.sender] = true; // default flag to post the tweet
     }
 
     function fetchAlluser() public view returns (User[] memory) {
         return users;
     }
 
-    function addTweet(string calldata content) public {
+    function addTweet(
+        string calldata content,
+        address _authorizedUser
+    ) public checkIfUserIsAuthorize(_authorizedUser) {
         require(bytes(content).length != 0, "Tweet cannot be empty");
         require(existingUsers[msg.sender], "User doesn't exist");
-        uint tc = tweetCount + 1;
-        tweets[msg.sender].push(
-            Tweet(tc, content, msg.sender, block.timestamp)
-        );
-        tweetCount = tc;
-    }
-
-    function getTweetByUser() public view returns (Tweet[] memory) {
-        return tweets[msg.sender];
+        uint tc = tweets.length + 1;
+        tweets.push(Tweet(tc, content, msg.sender, block.timestamp));
+        tweetCountByUser[msg.sender] = tweetCountByUser[msg.sender] + 1;
     }
 
     function getTweetCount() public view returns (uint) {
-        return tweetCount;
+        return tweets.length;
     }
 
-    function getTweetByCount(
+    // The latest tweets posted by users, considering a specified count.
+    function getLatestTweetByCount(
         uint _count
     )
         public
@@ -88,34 +99,109 @@ contract TwitterContract {
         checkIfTweetCountExceed(msg.sender, _count)
         returns (Tweet[] memory)
     {
-        // Tweet[] memory latestTweet = getTweetUtils(msg.sender, _count);
-        return getTweetUtils(msg.sender, _count);
+        uint tweetCount = tweets.length;
+        Tweet[] memory latestTweet = new Tweet[](_count);
+        uint j = 0;
+        for (uint index = tweetCount; j < _count; index--) {
+            Tweet memory ele = tweets[index - 1];
+            latestTweet[j] = ele;
+            j++;
+        }
+        return latestTweet;
     }
 
-    function getTweetByUserWithCount(
+    // Retrive the latest tweets of a specific user, considering a specified count.
+    // The latest tweets of a specific user, considering a specified count.
+    function getLatestTweetByUser(
         address _user,
         uint _count
     )
         public
         view
         checkIfTweetCountExceed(_user, _count)
+        checkIfUserDoesntExist(_user)
         returns (Tweet[] memory)
     {
-        return getTweetUtils(_user, _count);
-    }
-
-    function getTweetUtils(
-        address _user,
-        uint _count
-    ) private view returns (Tweet[] memory) {
-        uint totalCount = tweets[_user].length;
-        Tweet[] memory latestTweet = new Tweet[](totalCount);
+        // Check if user has number of tweets
+        uint tweetCountByUsers = tweetCountByUser[msg.sender];
+        require(
+            _count <= tweetCountByUsers,
+            "Your tweets are below the expected count."
+        );
+        uint tweetCount = tweets.length;
+        Tweet[] memory latestTweet = new Tweet[](_count);
         uint j = 0;
-        for (uint index = _count; index > 0; index--) {
-            Tweet memory ele = tweets[_user][index - 1];
-            latestTweet[j] = ele;
-            j++;
+        for (uint index = tweetCount; j < _count; index--) {
+            Tweet memory ele = tweets[index - 1];
+            if (ele.author == _user) {
+                latestTweet[j] = ele;
+                j++;
+            }
         }
         return latestTweet;
+    }
+
+    // FOLLOW users
+    // Develop a function that enables a user to follow another user by adding their address to a list of followed users.
+    function followUser(
+        address _followingUser
+    ) public checkIfUserDoesntExist(msg.sender) {
+        require(!existingUsers[_followingUser], "following user doesn't exist");
+        address[] storage followingUser = followings[msg.sender];
+        followingUser.push(_followingUser);
+        address[] storage followedUser = followers[_followingUser];
+        followedUser.push(msg.sender);
+    }
+
+    function fetchFollowers() public view returns (address[] memory) {
+        return followers[msg.sender];
+    }
+
+    // Implement a function that enables users to send messages to other users. It should record the sender, recipient, message content, and creation timestamp.
+    function sendMessage(
+        address _recipient,
+        string calldata _message,
+        address _authorizedUser
+    )
+        public
+        checkIfUserDoesntExist(msg.sender)
+        checkIfUserDoesntExist(_recipient)
+        checkIfUserIsAuthorize(_authorizedUser)
+    {
+        messages.push(
+            Message(msg.sender, _recipient, _message, block.timestamp)
+        );
+    }
+
+    function authorizeUserForTweet(
+        address _userAddress
+    )
+        public
+        checkIfUserDoesntExist(msg.sender)
+        checkIfUserDoesntExist(_userAddress)
+    {
+        accessControl[msg.sender][_userAddress] = true;
+    }
+
+    function unauthorizeUserForTweet(
+        address _userAddress
+    )
+        public
+        checkIfUserDoesntExist(msg.sender)
+        checkIfUserDoesntExist(_userAddress)
+    {
+        accessControl[msg.sender][_userAddress] = false;
+    }
+
+    function checkAuthorization(
+        address _userAddress
+    )
+        public
+        view
+        checkIfUserDoesntExist(msg.sender)
+        checkIfUserDoesntExist(_userAddress)
+        returns (bool)
+    {
+        return accessControl[msg.sender][_userAddress];
     }
 }
